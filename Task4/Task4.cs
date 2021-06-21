c480b7248960703387259c49dc46eadbcd68c2d1
CrazyOverdose <47750322+CrazyOverdose@users.noreply.github.com>
2021-06-21 20:37:59 +0300

    Update Task4.cs

--- Task4/Task4.cs
+++ Task4/Task4.cs
@@ -1,204 +1,276 @@
 using System;
-using System.Collections.Generic;
 
 namespace ИТИБ2
 {
 
-    class NeuralNetwork
+    class BackPropagation
     {
         /// <summary>
-        /// Конструктор класса Нейронная сеть 
+        /// Конструктор класса Алгоритма обратного распространения ошибки
         /// </summary>
-        /// <param name="p_">Размер окна</param>
-        /// <param name="a_">Левая граница отрезка</param>
-        /// <param name="b_">Правая граница отрезка</param>
-        /// <param name="N_">Количество точек</param>
-        /// <param name="TeachingRate_">Норма обучения</param>
-        public NeuralNetwork(int p_, double a_, double b_, int N_, double TeachingRate_)
+        /// <param name="N_,J_, M_">Архитектура</param>
+        /// <param name="x1_">Входной вектор</param>
+        /// <param name="t_">Целевой вектор</param>
+        /// <param name="n_">Норма обучения</param>
+        /// <param name="e_">Погрешность</param>
+        public BackPropagation(int N_, int J_, int M_, double[] x1_, double[] t_, double n_, double e_)
         {
-            p = p_; a = a_; b = b_; N = N_; TeachingRate = TeachingRate_;
+            N = N_; J = J_; M = M_; x1 = x1_; t = t_; n = n_; e = e_;
 
-            weights = new double[p+1];
-            deltaWeights = new double[p+1];
-            SubsequentInterval();
-            ComputingDiscreteSets();
+            net1 = new double[J + 1];
+            net2 = new double[M + 1];
+            x2 = new double[J + 1];
+
+            for (int i = 0; i < x2.Length; ++i)
+                x2[i] = 1;
+
+            y = new double[M + 1];
+            w1 = new double[N + 1,J + 1];
+            w2 = new double[J + 1,M + 1];
+            delta1 = new double[J + 1];
+            delta2 = new double[M + 1];
             TotalError();
-            Print();
-            
+        }
+
+        public void Life()
+        {   while (Error >= e)
+            {
+                Error = 0.0;
+                FirstStage();
+                SecondStage();
+                ThirdStage();
+                TotalError();
+                Print();
+                ++Epoch;
+            }
         }
 
         /// <summary>
-        /// Печатание каждой эпохи
+        /// Печать результата
         /// </summary>
         private void Print()
-        {
-            Console.Write("Эпоха: ");
-            Console.Write("{0,4}", $"{Epoch}");
-            Console.Write(" | Вектор весов: ");
-            foreach (double i in weights)
-            {
-                Console.Write("{0,7}", $"{Math.Round(i, 3).ToString("N3")} ");
-            }
-            Console.Write($" | Суммар. ошибка: ");
-            Console.Write("{0,2}", $"{Error}\n");
+        {         
+                Console.Write("Эпоха: ");
+                Console.Write("{0,3}", $"{Epoch}");
+                Console.Write($" | Вых. вектор у: ");
+                for (var  i = 1; i < y.Length; ++i)
+                {
+                    Console.Write("{0,7}", $"{Math.Round(y[i], 5).ToString("N5")} ");
+                }
+                Console.Write($"| Цел. вектор t = ");
+                for (var i = 1; i < t.Length; ++i)
+                {
+                    Console.Write("{0,3}", $"{Math.Round(t[i], 1).ToString("N1")} ");
+                }
+                Console.Write($"| Сум. ошибка: ");
+                Console.Write("{0,4}", $"{Math.Round(Error, 5).ToString("N5") }");
+                Console.Write(" | Веса нейронов скрыт. и выход. слоев: w(1) = ");
+
+                for (var j = 1; j <= J; ++j)
+                {
+                    for (var i = 0; i <= N; ++i)
+                    {
+                        Console.Write("{0,4}", $"{Math.Round(w1[i,j], 2).ToString("N2")} ");
+                    }
+                }
+
+                Console.Write("w(2) = ");
+                for (var j = 0; j <= J; ++j)
+                {
+                    for (var m = 1; m <= M; ++m)
+                    {
+                        Console.Write("{0,5}", $"{Math.Round(w2[j, m], 5).ToString("N5")} ");
+                    }
+                }
+            
+                Console.Write("\n\n");
         }
-        
+
         /// <summary>
-        /// Обучение + прогноз нейронной сети
+        /// Расчет функции активации нейронов скрытого и выходного слоев
         /// </summary>
-        /// <param name="MaxEpoch"> Максимальное количество эпох</param>
-        public void Life(int MaxEpoch)
+        private double f_net(double net)
         {
-            while (Epoch < MaxEpoch)
-            {
-                SlidingWindow();
-                TotalError();
-                ++Epoch;
-                Print();
-            };
-            Forecasting();
+            return (1.0 - Math.Exp(-1.0 * net)) / (1.0 + Math.Exp(-1.0 * net));
         }
 
         /// <summary>
-        /// Прогнозирование нейронной сети
+        /// Расчет производной функции активации нейронов скрытого и выходного слоев
         /// </summary>
-        private void Forecasting()
+        private double df_net(double net)
         {
-            for (int i = (N - p - 1); i <= (2*N - p - 2); ++i)
-            {
-                var PredictedValue = CalculationX_t(i);
-
-                if (i == N - p - 1) // замена значения функции в точке конца отрезка обучения и начала отрезка прогнозирования
-                    xt[N - 1] = PredictedValue;
-                else
-                    xt.Add(PredictedValue);
-                Forecast.Add(new KeyValuePair<double, double>(PredictedValue, x_coordinates[i+p]));
-            }
-            Console.Write($"\nПрогноз на интервале [{b}, {c}]:");
-
-            for (var i = 0; i < Forecast.Count; ++i)
-            {
-                Console.Write("\n(");
-                Console.Write("{0,16}", Forecast[i].Value.ToString("N14"));
-                Console.Write("; ");
-                Console.Write("{0,16}", Forecast[i].Key.ToString("N14"));
-                Console.Write(")");
-            }
-            Console.WriteLine();
-            Forecast.Clear();
+            return 0.5* (1 - f_net(net)* f_net(net));
         }
 
         /// <summary>
-        /// Метод "скользящего окна"
+        /// Расчет комбинированного входа нейронов скрытого слоя
         /// </summary>
-        private void SlidingWindow()
+        private void CalculationNet1()
         {
-            for(int i = 0; i <= (N - p - 1); ++i)
+            for (var j = 1; j <= J; ++j)
             {
-                WidrowHoff(i);
+                var net = 0.0;
+                for (var i = 1; i <= N; ++i)
+                {
+                    net += w1[i, j] * x1[i];
+                }
+                net1[j] = net + w1[0, j];
             }
-        }
-
-        /// <summary> Пересчитывания весов для каждого набора Х</summary>
-        /// /// <param name="index">Индекс Х, c которого начинается окно</param>
-        private void WidrowHoff(int index)
-        {
-            double x_t = CalculationX_t(index);
-            for (int j = 1; j <= p; ++j)
-            {
-                deltaWeights[j] = TeachingRate * (xt[index + p] - x_t) * xt[index + j -1];
-                weights[j] += deltaWeights[j];
-            }
-          
-        }
-
-        /// <summary> Расчет последующего интервала</summary>
-        private void SubsequentInterval()
-        {
-            c = 2 * b - a;
-        }
-
-        ~NeuralNetwork() { }
-
-        /// <summary> Так как расположение точек равномерное, то следует 
-        /// расчитать разность арифметической прогрессии</summary>
-        /// <param name="max">Правая граница отрезка</param>
-        /// <param name="min">Левая граница отрезка</param>
-        private void СalculateDifferenceProgress(double max, double min)
-        {
-            DifferenceProgress = Math.Sqrt((max - min) * (max - min)) / (2*N-2.0);
-        }
-
-        /// <summary> Высчитывание дискретных наборов значений функции x(t)</summary>
-        private void ComputingDiscreteSets()
-        {
-            СalculateDifferenceProgress(c, a);
-            double t = a;
-            for(double i = 0; i < 2*N - 1; ++i)
-            {
-                if( i <= N - 1 ) // координаты х нужны на весь отрезок [a,c], а координаты у только на [a,b]
-                    xt.Add(X_tFunction(t));
-                x_coordinates.Add(t);
-                t += DifferenceProgress;
-            }
-        }
-
-        /// <summary> Функция x(t)</summary>
-        /// <param name="t">Момент времени</param>
-        private double X_tFunction(double t)
-        {
-            return t * t * Math.Sin(t);
-        }
-
-        /// <summary> Высчитывание X~(t)</summary>
-        /// <param name="index">Индекс Х, c которого начинается окно</param>
-        private double CalculationX_t(int index)
-        {
-            double x_t = 0;
-            for (int k = 1; k <= p; ++k)
-            {
-                x_t += (weights[k]*xt[index + k-1]);
-            }
-            return x_t + weights[0];
         }
 
         /// <summary>
-        /// Расчет суммарной квадратичной ошибки в конце эпохи
+        /// Расчет комбинированного входа нейронов выходного слоя
+        /// </summary>
+        private void CalculationNet2()
+        {
+            for (var m = 1; m <= M; ++m)
+            {
+                var net = 0.0;
+                for (var j = 1; j <= J; ++j)
+                {
+                    net += w2[j, m] * x2[j];
+                }
+                net2[m] = net +  w2[0, m];
+            }
+        }
+
+        /// <summary>
+        /// Расчет входного сигнала нейронов выходного слоя
+        /// </summary>
+        private void CalculationX2()
+        {
+            for (var j = 1; j <= J; ++j)
+            {
+                x2[j] = f_net(net1[j]);
+            }
+        }
+
+        /// <summary>
+        /// Расчет выхода многослойной нейронной сети 
+        /// </summary>
+        private void CalculationYm()
+        {
+            for(var m = 1; m <= M; ++m )
+            {
+                y[m] = f_net(net2[m]);
+            }
+        }
+
+        private void FirstStage()
+        {
+            CalculationNet1();
+            CalculationX2();
+            CalculationNet2();
+            CalculationYm();
+        }
+
+        /// <summary>
+        /// Расчет ошибки выходного слоя
+        /// </summary>
+        private void Calculationdelta()
+        {
+            for (var m = 1; m <= M; ++m)
+            {
+                delta2[m] = df_net(net2[m])*(t[m] - y[m]);
+            }
+        }
+
+        /// <summary>
+        /// Расчет ошибки скрытого слоя
+        /// </summary>
+        private void Calculationdelta1()
+        {
+            for (var j = 1; j <= J; ++j)
+            {
+                var sum = 0.0;
+                for (var m = 1; m <= M; ++m)
+                {
+                    sum += w2[j, m] * delta2[m];
+                }
+
+                delta1[j] = sum * df_net(net1[j]);
+            }
+        }
+
+        private void SecondStage()
+        {
+            Calculationdelta();
+            Calculationdelta1();
+        }
+
+       /// <summary>
+       /// Настройка весов нейронов скрытого слоя
+       /// </summary>
+        private void Calculationw1()
+        {
+            for (var j = 1; j <= J; ++j)
+            {
+                var deltaw = 0.0;
+                for (var i = 0; i <= N; ++i)
+                {
+                    deltaw = n * x1[i]*delta1[j];
+                    w1[i,j] += deltaw;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Настройка весов нейронов выходного слоя
+        /// </summary>
+        private void Calculationw2()
+        {
+            for (var j = 0; j <= J; ++j)
+            {
+                var deltaw = 0.0;
+                for (var m = 1; m <= M; ++m)
+                {
+                    deltaw = n * x2[j] * delta2[m];
+                    w2[j, m] += deltaw;
+                }
+            }
+        }
+
+        private void ThirdStage()
+        {
+            Calculationw1();
+            Calculationw2();
+        }
+
+        /// <summary>
+        ///Расчет суммарной среднеквадратичной ошибки
         /// </summary>
         private void TotalError()
         {
-            Error = 0;
-            for (int i = 0; i <= (N - p - 1); ++i)
+            for (var j = 1; j <= M; ++j)
             {
-                double delta = xt[i + p] - CalculationX_t(i);
-                Error += delta * delta;
+                Error += (t[j] - y[j])*(t[j] - y[j]); 
             }
 
             Error = Math.Sqrt(Error);
         }
+        
 
-        private int p; // длина окна
-        private double a, b; // границы временного интервала
-        private double c; // граница справа последующего интервала
-        private int N; // количество точек 
-        private List <double> xt = new List<double>(); // дискретный набор значений функции
-        // Спрогнозированные значения функции + координата по по оси x
-        private List<KeyValuePair<double, double>> Forecast = new List<KeyValuePair<double, double>>(); 
-        private List<double> x_coordinates  = new List<double>();
-        private double[] weights; // Веса
-        private double TeachingRate; // Норма обучения
-        private double DifferenceProgress; // Разность арифметической погрессии (расположение точек на отрезке равномерное)
-        private int Epoch = 0; // Номер эпохи
-        private double[] deltaWeights; // Дельты весов
-        private double Error; // Cуммарная квадратичная ошибка в конце эпохи
+        private readonly int N, J, M; // N-J-M - архитектура
+        private readonly double n; //Норма обучения
+        private double Error = 0.0; // Cуммарная среднеквадратичная ошибка
+        private int Epoch = 0; //Номер эпохи
+        private double e; //Погрешность
+        private double[] t; //Целевой вектор
+        private double[] x2; //Входной сигнал нейронов выходного слоя
+        private double[] net1, net2; //Комбинированные входы нейронов скрытого слоя
+        private double[] y; //Выход многослойной нейронной сети 
+        private double[] delta2, delta1; // Ошибка скрытого и выходного слоев
+        private double[] x1; //Входной вектор
+        private double[,] w1, w2; //Веса нейронов скрытого и выходного слоев
     }
 
-        class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            NeuralNetwork my = new NeuralNetwork(9, -1, 1, 20, 1);
-            my.Life(5000);
+            //в массиве t всегда первый элемент = 0
+            BackPropagation my = new BackPropagation(1, 1, 3, new[] { 1.0, -1.0}, new[] {0.0, -1.0, 2.0, 2.0 }, 1, 1.42);
+            my.Life();
         }
     }
 }
